61b6e037548f64bca98069227f7172c20c6bc51a
Maya Franchi <67254101+mFranchi92@users.noreply.github.com>
2022-07-30 17:29:39 -0300

    routing

--- src/main/java/com/example/demo/controller/EduController.java
+++ src/main/java/com/example/demo/controller/EduController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("educacion")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://apfrontproj.web.app/")
 public class EduController {
     @Autowired
     SEducacion sEducacion;
--- src/main/java/com/example/demo/controller/ExpController.java
+++ src/main/java/com/example/demo/controller/ExpController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://apfrontproj.web.app/")
 public class ExpController {
     @Autowired
     SExperiencia sExperiencia;
